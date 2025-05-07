3ab800748b91e484631d2b2651006162cd9eff2d
Dragunfrut <dragunfrut3rdp@gmail.com>
2025-05-07 17:05:27 -0400

    something funny happened

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const sawanoAudio = document.getElementById("sawano");
 const crawlIMG = document.getElementById("crawl");
 
 crawlButton.addEventListener("click", () => {
-    if(crawlIMG.style.visibility == "hidden") {
-        crawlIMG.style.visibility = "visible";
-        sawanoAudio.play();
-        sawanoAudio.currentTime = 36;
-    }
-    else{
-        crawlIMG.style.visibility = "hidden";
+    if(crawlIMG.className === "in") {
+        crawlIMG.className = "out";
         sawanoAudio.pause();
-        sawanoAudio.currentTime = 36;
+    }
+    else {
+        crawlIMG.className = "in";
+        sawanoAudio.play();
+        sawanoAudio.currentTime = 37;
+    }
+    // if(crawlIMG.style.visibility == "hidden") {
+    //     crawlIMG.style.visibility = "visible";
+    //     sawanoAudio.play();
+    //     sawanoAudio.currentTime = 36;
+    // }
+    // else{
+    //     crawlIMG.style.visibility = "hidden";
+    //     sawanoAudio.pause();
+    // }
+})
+
+var img = document.getElementById("dog"),
+    btn = document.getElementById("button");
+
+btn.addEventListener("click", () => {
+    if(img.className === "in") {
+        img.className = "out";
+    }
+    else {
+        img.className = "in";
     }
 })
