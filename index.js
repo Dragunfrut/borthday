d92bd69e75636121189e0b39e463d66336bf4542
Dragunfrut <dragunfrut3rdp@gmail.com>
2025-07-10 17:26:35 -0400

    something happened

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const crawlButton = document.getElementById("crawlButton");
 const sawanoAudio = document.getElementById("sawano");
 const crawlIMG = document.getElementById("crawl");
+const image = document.querySelector(".image");
 
 crawlButton.addEventListener("click", () => {
     if(crawlIMG.className === "in") {
         crawlIMG.className = "out";
+        image.style.height = "0";
         sawanoAudio.pause();
     }
     else {
         crawlIMG.className = "in";
+        image.style.height = "100%";
         sawanoAudio.play();
         sawanoAudio.currentTime = 37;
     }
