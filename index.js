b288eb3c4b6ddeeebd4c5b5a98edfb30266aaac1
Dragunfrut <dragunfrut3rdp@gmail.com>
2025-08-01 17:04:13 -0400

    something happened

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const crawlButton = document.getElementById("crawlButton");
 const sawanoAudio = document.getElementById("sawano");
 const crawlIMG = document.getElementById("crawl");
 const image = document.querySelector(".image");
-const birthday = document.querySelector(".birthday")
+const birthdayButton = document.getElementById("birthdayButton")
+const birthdayAudio = document.getElementById("birthday")
+const fireIMG = document.getElementById("fire")
 
 crawlButton.addEventListener("click", () => {
     if(crawlIMG.className === "in") {
@@ -14,7 +16,19 @@ crawlButton.addEventListener("click", () => {
         crawlIMG.className = "in";
         image.style.height = "100%";
         sawanoAudio.play();
-        sawanoAudio.currentTime = 37;
+        sawanoAudio.currentTime = 0;
     }
 })
 
+birthdayButton.addEventListener("click", () => {
+    if(fireIMG.className === "in") {
+        fireIMG.className = "out";
+        image.style.height = "0";
+        birthdayAudio.pause();
+    }
+    else {
+        fireIMG.className = "in";
+        birthdayAudio.play();
+        birthdayAudio.currentTime = 0;
+    }
+})
